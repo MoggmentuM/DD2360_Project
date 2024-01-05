b92f32b41a8d6260d1764d7dc39dc226aa8618a2
shiruimin123 <3237178795@qq.com>
2024-01-05 15:28:10 +0100

    new_version_stream_float

--- project/float_stream/ex_particle_CUDA_float_stream.cu
+++ project/float_stream/ex_particle_CUDA_float_stream.cu
@@ -757,9 +757,10 @@ void particleFilter(unsigned char * I, int IszX, int IszY, int Nfr, int * seed, 
         int offset = i * SEGMENT_SIZE;
         
         likelihood_kernel << < num_blocks, threads_per_block ,threads_per_block,streams[i]>> > (arrayX_GPU, arrayY_GPU, xj_GPU, yj_GPU, CDF_GPU, ind_GPU, objxy_GPU, likelihood_GPU, I_GPU, u_GPU, weights_GPU, Nparticles, countOnes, max_size, k, IszY, Nfr, seed_GPU, partial_sums,offset,SEGMENT_SIZE);
-        printf("check error");
+        
 
         sum_kernel << < num_blocks, threads_per_block,0,streams[0] >> > (partial_sums, Nparticles);
+        printf("check error111");
 
         normalize_weights_kernel << < num_blocks, threads_per_block,0,streams[i] >> > (weights_GPU, Nparticles, partial_sums, CDF_GPU, u_GPU, seed_GPU,offset,SEGMENT_SIZE);
         
