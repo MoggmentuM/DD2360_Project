f75aefb96f61a1c81eb1a6edf0d715b578570d35
shiruimin123 <3237178795@qq.com>
2024-01-08 00:01:33 +0100

    png nstream

--- project/float_stream/ex_particle_CUDA_float_stream.cu
+++ project/float_stream/ex_particle_CUDA_float_stream.cu
@@ -379,7 +379,7 @@ __global__ void likelihood_kernel(double * arrayX, double * arrayY, double * xj,
 
 __global__ void sum1(double * weights, double *partial_sums,int Nparticles){
     int block_id = blockIdx.x;
-    int i = blockDim.x * block_id + threadIdx.x + offset;
+    int i = blockDim.x * block_id + threadIdx.x ;
 
     extern __shared__ double buffer[];
 
